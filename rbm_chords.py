76098f7d11e6312cd7ac5938f132287560bf2d62
Raffaele Del Prete <raffo002@gmail.com>
2019-02-21 14:53:06 +0100

    Fix

--- rbm_chords.py
+++ rbm_chords.py
@@ -16,7 +16,7 @@ import midi_manipulation
 
 def get_songs(path):
     files = glob.glob('{}/*.mid*'.format(path))
-    print file
+    print (file)
     songs = []
     for f in tqdm(files):
         try:
